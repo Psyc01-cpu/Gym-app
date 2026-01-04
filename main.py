12f4a76d8055d3ab7540aa69d382527e04be34cf
Psyc01-cpu <jordan.h974@hotmail.fr>
2026-01-04 21:53:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import Body, HTTPException
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
