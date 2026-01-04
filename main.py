8cde1b27085d81f069ab2c401eb6adf53536ec95
Psyc01-cpu <jordan.h974@hotmail.fr>
2026-01-04 20:41:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI()
 
-# fichiers statiques (css / js)
+# Lien vers les fichiers statiques (CSS / JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# templates HTML
+# Lien vers les templates HTML
 templates = Jinja2Templates(directory="templates")
 
-
+# Page principale
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse(
